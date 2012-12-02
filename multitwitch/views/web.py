d6639a86baf601c479b92412649ec5336142e72d
Brian C Hamrick <bhamrick@mit.edu>
2012-12-01 21:24:33 -0500

    Testing remainder match

--- multitwitch/views/web.py
+++ multitwitch/views/web.py
@@ -3,8 +3,6 @@ from multitwitch.lib.session import web, ajax
 class WebView:
     @web(template="web/home.tmpl")
     def home(request):
-        return {'project' : 'multitwitch'}
-
-    @ajax()
-    def ajax_test(request):
-        return [1, {'a' : 2, 'b' : 4}, 3]
+        streams = request.matchdict[streams]
+        return {'project' : 'multitwitch',
+                'args' : 'streams'}
