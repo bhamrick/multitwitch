a19e3ff6ecb0110f1a8fd47df1996bee96ec037c
Brian C Hamrick <bhamrick@mit.edu>
2012-12-01 21:26:00 -0500

    Make string

--- multitwitch/views/web.py
+++ multitwitch/views/web.py
@@ -3,6 +3,6 @@ from multitwitch.lib.session import web, ajax
 class WebView:
     @web(template="web/home.tmpl")
     def home(request):
-        streams = request.matchdict[streams]
+        streams = request.matchdict['streams']
         return {'project' : 'multitwitch',
                 'args' : 'streams'}
