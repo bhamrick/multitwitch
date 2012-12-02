4a5c775f33ab2411ed0e1f04c9abf3ca5d9fe645
Brian C Hamrick <bhamrick@mit.edu>
2012-12-01 20:33:50 -0500

    Removed files that shouldnt exist

--- APPNAME/static/js/split_test.js
+++
@@ -1,9 +0,0 @@
-var root_frame = null;
-
-$(document).ready(function() {
-    root_frame = new Frame(null);
-    root_frame.split_direction="v";
-    root_frame.children.push(new Frame(root_frame));
-    root_frame.children.push(new Frame(root_frame));
-    Resize(root_frame, $(window).width(), $(window).height());
-});
