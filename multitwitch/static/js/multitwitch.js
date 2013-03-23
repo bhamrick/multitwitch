1c70841488f8d818f84611419b5b6580379d2d66
Brian C Hamrick <bhamrick@mit.edu>
2013-03-23 03:41:41 -0400

    Round down sizes

--- multitwitch/static/js/multitwitch.js
+++ multitwitch/static/js/multitwitch.js
@@ -33,8 +33,8 @@ function optimize_size(n) {
             wrapper_padding = (height - num_rows * max_height)/2;
         }
     }
-    $(".stream").height(best_height);
-    $(".stream").width(best_width);
+    $(".stream").height(floor(best_height));
+    $(".stream").width(floor(best_width));
     $("#streams").css("padding-top", wrapper_padding);
 }
 
