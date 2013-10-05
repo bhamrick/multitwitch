8eb916639b1e71d7f4a5e2083def5ab6da2b6b6f
Brian Hamrick <brian.c.hamrick@gmail.com>
2013-10-04 18:10:50 -0700

    Preserve order of streams in chat box

--- multitwitch/views/web.py
+++ multitwitch/views/web.py
@@ -4,7 +4,11 @@ class WebView:
     @web(template="web/home.tmpl")
     def home(request):
         streams = request.matchdict['streams']
+        uniq_streams = []
+        for s in streams:
+            if s not in uniq_streams:
+                uniq_streams.append(s)
         return {'project' : 'multitwitch',
                 'streams' : streams,
-                'unique_streams' : set(streams),
+                'unique_streams' : uniq_streams,
                 'nstreams' : len(streams)}
