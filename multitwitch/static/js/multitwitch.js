838debe6e2cc9034fed862804c6458da7939183d
Brian Hamrick <brian.c.hamrick@gmail.com>
2016-08-06 12:28:51 -0700

    Remove consideration for 30 px bottom bar

--- multitwitch/static/js/multitwitch.js
+++ multitwitch/static/js/multitwitch.js
@@ -50,10 +50,10 @@ function optimize_size(n) {
         var num_rows = Math.ceil(n / per_row);
         var max_width = Math.floor(width / per_row) - 4;
         var max_height = Math.floor(height / num_rows) - 4;
-        if (max_width * 9/16 + 30 < max_height) {
-            max_height = max_width * 9/16 + 30;
+        if (max_width * 9/16 < max_height) {
+            max_height = max_width * 9/16;
         } else {
-            max_width = (max_height - 30) * 16/9;
+            max_width = (max_height) * 16/9;
         }
         if (max_width > best_width) {
             best_width = max_width;
